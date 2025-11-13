57b1be3848eb24fa92123d132426c887301c75ae
tW3im <119765991+TWim3@users.noreply.github.com>
2025-11-13 21:30:31 +0100

    temp convertion

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use embassy_stm32::peripherals::ADC1;
 use embassy_stm32::{adc, bind_interrupts};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
+use pt_rtd::*;
 
 bind_interrupts!(struct Irqs {
     ADC1_2 => adc::InterruptHandler<ADC1>;
@@ -19,14 +20,19 @@ async fn main(_spawner: Spawner) {
     info!("Hello World!");
 
     let mut adc = Adc::new(p.ADC1);
-    let mut pin = p.PB1;
+    let mut pin = p.PA3;
 
 
     adc.set_sample_time(adc::SampleTime::CYCLES41_5);
 
     loop {
         let v = adc.read(&mut pin).await;
-        info!("--> {} ", v);
-        Timer::after_millis(100).await;
+        info!("--> {} ", calculate_temp(v));
+        Timer::after_millis(1000).await;
     }
+}
+
+fn calculate_temp(v: u16) -> u16 {
+    let res = (2.2_f32 * 1000.0) * ((v as f32 / 4096.0) - v as f32);
+    calc_t(res , RTDType::PT1000).unwrap() as u16
 }
