c6ddc259d2c130591f18da29acb01eeabf8a99b5
tW3im <119765991+TWim3@users.noreply.github.com>
2025-11-13 19:50:05 +0100

    adc test

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,24 @@
 
 mod fmt;
 
-#[cfg(not(feature = "defmt"))]
-use panic_halt as _;
 #[cfg(feature = "defmt")]
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
-use embassy_time::{Duration, Instant, Timer};
+use embassy_stm32::adc::{Adc};
+use embassy_time::{Timer};
 use fmt::info;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
 
+        let mut temp_adc = Adc::new(p.ADC1);
+        let mut temp_pin = p.PA3;
+
     loop {
-        info!("Hello, World!");
-
-        let inst = Instant::now();
-
-        info!("{}", inst.as_secs());
+        let measurement = temp_adc.read(&mut temp_pin).await;
+        info!("temperature: {}", measurement);
+        Timer::after_millis(1000).await;
     }
 }
