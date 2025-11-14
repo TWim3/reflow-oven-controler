4e5d0d3fa7c5c1621c0bad7e10c90723f9d23620
Hopfenschorsch <159639368+Hopfenschorsch@users.noreply.github.com>
2025-11-14 11:06:55 +0100

    Kommentare hinzugefügt.

--- src/main.rs
+++ src/main.rs
@@ -17,40 +17,44 @@ bind_interrupts!(struct Irqs {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+    info!("Connected");
+//Definition ADC + Pins
 
     let mut adc = Adc::new(p.ADC1);
     let mut pin = p.PA3;
 
-
   adc.set_sample_time(adc::SampleTime::CYCLES239_5);
 
     loop {
-        // Anzahl der Samples für Oversampling.
+//Anzahl der Samples für Oversampling.
         const OVERSAMPLE_COUNT: u32 = 256;
 
         let mut acc: u32 = 0;
 
         for _ in 0..OVERSAMPLE_COUNT {
-        //ADC-Wert lesen und aufaddieren
+//ADC-Wert lesen und aufaddieren
+
             acc += adc.read(&mut pin).await as u32;
         }
 
-        // Oversampling-Skalierung:
-        // Die Summe enthält 12 Bit (ADC) + 8 Bit (256 Samples) = 20 Bit Information.
-        // Wir wollen effektiv 16 Bit behalten → 20 - 16 = 4 Bit wegschieben.
-        // Daher: Rechts-Shiften um 4 Bit.
+// Oversampling
+// Rechts-Shiften um 4 Bit.
+
         let value: u16 = (acc >> 4) as u16;
 
+//Output Temperature
         match calculate_temp(value) {
             Ok(temp) => info!("Temperature: {} °C Volt: {}", temp,value),
             Err(_) => error!("Error calculating temperature"),
         }
-        Timer::after_millis(1000).await;
+//Timer for Output
+        Timer::after_millis(500).await;
     }
 }
-
+//Temperatur Umwandlung
 fn calculate_temp(v: u16) -> Result<f32, Error> {
+//ADC Value to Resistance
     let res = ((2.2_f32 * 1000.0) * v as f32 )/( 4096.0 - v as f32);
+//Resistance to Temperature
     calc_t(res , RTDType::PT1000)
 }
