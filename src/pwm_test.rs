809e7941605db7123fc57b024855969fc279cfcd
tW3im <119765991+TWim3@users.noreply.github.com>
2025-11-17 20:01:22 +0100

    test input capture

--- src/pwm_test.rs
+++ src/pwm_test.rs
@@ -5,6 +5,7 @@ use embassy_stm32::peripherals::{PA7, TIM3};
 use embassy_stm32::time::khz;
 use embassy_stm32::timer::Channel;
 use embassy_stm32::timer::input_capture::{CapturePin, InputCapture};
+use embassy_time::Instant;
 
 bind_interrupts!(struct Irqs {
     TIM3 => timer::CaptureCompareInterruptHandler<peripherals::TIM3>;
@@ -16,7 +17,7 @@ pub async fn pwm_test(signal_input: Peri<'static, PA7>, tim3: Peri<'static, TIM3
     // Implement PWM functionality tests here.
 
     let ch2 = CapturePin::new(signal_input, Pull::None);
-    let mut ic = InputCapture::new(
+    let ic = InputCapture::new(
         tim3,
         None,
         Some(ch2),
@@ -27,12 +28,12 @@ pub async fn pwm_test(signal_input: Peri<'static, PA7>, tim3: Peri<'static, TIM3
         Default::default(),
     );
 
-    loop {
-        info!("wait for rising edge");
-        ic.wait_for_rising_edge(Channel::Ch2).await;
+    let timer = Instant::now();
 
-        let capture_value = ic.get_capture_value(Channel::Ch2);
-        info!("new capture! {}", capture_value);
+    loop {
+        if(ic.get_input_interrupt(Channel::Ch2)) {
+            info!("Interrupt received at {:?}", timer.as_millis());
+        }
     }
 }
 
