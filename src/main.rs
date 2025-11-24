d9dcdf24c52a70071fc4db2d72fe619294547855
tW3im <119765991+TWim3@users.noreply.github.com>
2025-11-24 20:12:09 +0100

    Dont start task twice

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ async fn main(_spawner: Spawner) {
             should_run = true;
         }
 
-        if stop_button.is_high() {
+        if stop_button.is_low() {
             Timer::after_millis(50).await;
             info!("Stopping oven task...");
 
